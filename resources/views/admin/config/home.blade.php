c3df459312c5be35cd879d79bb72ac1e2cf32ae9
John Ciesluk <john.ciesluk@gmail.com>
2018-01-03 15:40:41 -0500

    Added csrf

--- resources/views/admin/config/home.blade.php
+++ resources/views/admin/config/home.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 <h2 class="text-center">Update Home Page</h2>
 <form class="form-horizontal" method="post" action="/admin/config/postHome">
+	{!! csrf_field() !!}
 	<div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
 	    <label for="description" class="col-sm-2 control-label">Description</label>
 	    <div class="col-sm-10">
