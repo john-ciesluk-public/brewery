b2f5b925091fd0f1c5c82ee965a9e8bf1780b343
John Ciesluk <john.ciesluk@gmail.com>
2018-01-05 16:43:10 -0500

    added vue todos

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,11 @@ Vue.component('event-modal', require('./components/EventModal.vue'));
 
 Vue.component('age-modal', require('./components/AgeModal.vue'));
 
+/**
+    TODO:
+    add vue2-quill-editor to admin pages
+    add vue2-multiselect to beer admin page (create/update hops/malts)
+*/
 const app = new Vue({
     el: '#app'
 });
